ac8df43adaa1949a768b8a96b61e12a0392c829d
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:32:36 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan52/siapa/kamu/Dosen.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan52.siapa.kamu;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Siapa Kamu
+ */
+public class Dosen extends Manusia{
+    private String nip,mataKuliah;
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getMataKuliah() {
+        return mataKuliah;
+    }
+
+    public void setMataKuliah(String mataKuliah) {
+        this.mataKuliah = mataKuliah;
+    }
+    
+    public void mengajarApa() {
+        System.out.println("Saya " + getNama() + " umur " + getUmur() + " tahun sedang mengajar matakuliah " + getMataKuliah());
+    }
+    
+    @Override
+    public void siapaKamu() {
+        System.out.println("Saya Dosen");
+    }
+}
